7bda1dc647f7147adc729cc2dab95a11470ea90b
oliver <oliver556@163.com>
2021-03-09 15:00:51 +0800

    feat： 1. 新增 type 类型判断函数

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-export { default as goodbye } from "./goodbye";
+/**
+ * @Description: 集合导出所有函数工具类
+ * @author: 李杰敏 on 2021/3/9
+ * @modify: 李杰敏 on 2021/3/9
+ */
 
+// type 类型判断
+export { default as isTypeof } from "./type/typeof";
+
+// 测试文件
+export { default as goodbye } from "./goodbye";
+// 测试文件
 import { hello } from './hello';
 export {
   hello
